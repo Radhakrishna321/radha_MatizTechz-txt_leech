a44085f8b49f6a78b92b0c148c9cd4e5bf01b7c1
Radhakrishna321 <167267393+Radhakrishna321@users.noreply.github.com>
2024-04-28 11:58:41 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27506873"
 api_hash = "a6e35f157348c804affcadece912fd30"
-bot_token = "6620259097:AAHOVxrpXfp63v0N54L3ljmKEJF_uR_isdk"
+bot_token = "6739651678:AAE5o8xZ4MgKhn0JHh6e_1IRn7bWE14AxGs"
